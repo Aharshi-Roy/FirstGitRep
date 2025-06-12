992ab73de342850a437956488aaa5f4386d67ea3
Aharshi-Roy <aharshi.roy@icloud.com>
2025-06-12 17:30:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let pic = document.getElementById("picture");
 
+function change_pic()
+{
+  let pic_num = Math.floor(Math.random() * 3);
+  let URL;
+  if (pic_num == 0) URL = "rook.png";
+  else if (pic_num == 1) URL = "pawn.png";
+  else if (pic_num == 2) URL = "knight.png"
+  pic.innerHTML = "<img src=\"" + URL + "\" alt=\"Chess peice\" width=\"30\" height=\"20\">";
+}
